ae6ff1241ebd59e0f8de0d408784713a2935d840
Jason Wright <noobjey@gmail.com>
2015-09-08 14:56:58 -0600

    added simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'simplecov'
+SimpleCov.start 'rails'
 
 ActiveRecord::Migration.maintain_test_schema!
 
